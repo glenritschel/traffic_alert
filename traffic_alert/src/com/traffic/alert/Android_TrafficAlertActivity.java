b6e523766e27727e0393f59d728cb0befbed26ca
Glen Ritschel <glen.ritschel@gmail.com>
2012-07-05 11:37:19 -0400

    Fixed manifest to have launcher, added main Java file.

---
+++ traffic_alert/src/com/traffic/alert/Android_TrafficAlertActivity.java
@@ -0,0 +1,14 @@
+package com.traffic.alert;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+	public class Android_TrafficAlertActivity extends Activity {
+	    /** Called when the activity is first created. */
+	    @Override
+	    public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.main);
+	    }
+
+}
